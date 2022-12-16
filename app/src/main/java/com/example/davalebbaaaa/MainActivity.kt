89de945f4eb707004a4de1c9148f243becf0e04f
Ani Kvernadze <ana.kvernadze.1@btu.edu.ge>
2022-12-16 12:17:05 +0400

    Initial commit

---
+++ app/src/main/java/com/example/davalebbaaaa/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.davalebbaaaa
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var emailAddress: EditText
+    private lateinit var password: EditText
+    private lateinit var button: Button
+    lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        emailAddress = findViewById(R.id.EmailAddress)
+        password = findViewById(R.id.textPassword)
+        button = findViewById(R.id.button)
+        auth = FirebaseAuth.getInstance()
+
+        button.setOnClickListener {
+
+            auth.createUserWithEmailAndPassword(emailAddress.text.toString(), password.text.toString())
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Toast.makeText(this,"წარმატებით შეიქმნა!", Toast.LENGTH_SHORT).show()
+
+                    }else{
+                        Toast.makeText(this, "ვერ შეიქმნა!")
+                    }
+                }
+        }
+
+    }
+
+}
